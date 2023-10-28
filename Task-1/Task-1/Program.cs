53d49d0ce4cefb76ef621bcb15aaab872452753f
Ruslan Seferov <91841957+ruslansfrov236@users.noreply.github.com>
2023-10-28 09:59:25 +0400

    Add files via upload
    C#

---
+++ Task-1/Task-1/Program.cs
@@ -0,0 +1,99 @@
+﻿//1 ci task
+//int m;
+//int ededi_Orta = 0;
+//int toplam = 0;
+//double total;
+//Console.WriteLine("Eded daxil edin :");
+
+//m=int.Parse(Console.ReadLine());
+
+
+
+//for(int i = 1; i < m; i++)
+//{
+//     if(i%21 == 0)
+//    {
+//        Console.WriteLine(i);
+//        ededi_Orta++;
+//        toplam += i;
+
+
+
+//    }
+
+
+//}
+//total = toplam / ededi_Orta;
+//Console.WriteLine($"{toplam}/{ededi_Orta}={total}");
+
+
+//2 ci task
+
+
+//int m, n;
+//int total = 0;
+//Console.WriteLine("Eded daxil edin : \t");
+//m=int.Parse(Console.ReadLine());
+//Console.WriteLine("Eded daxil edin : \t");
+//n = int.Parse(Console.ReadLine());
+
+//if (m < n)
+//    for (int i = m; i < n; i++)
+//        if(i%2 == 0)
+//        total += i;
+//if (m > n)
+//    for (int i = n; i < m; i++)
+//        if (i % 2 == 0)
+//            total += i;
+
+//Console.WriteLine($"Daxil olan cut ededlerin cemi:{total}");
+
+
+//3 cu task
+
+//int m, n, j, i;
+//int total = 0;
+
+//Console.Write("M-daxil edin: ");
+//m = int.Parse(Console.ReadLine());
+//Console.Write("N-daxil edin: ");
+//n = int.Parse(Console.ReadLine());
+
+
+//if (m < n)
+//{
+//    for (i = m; i < n; i++)
+
+
+//        total += i;
+
+
+//    for (j = 1; j < total; j++)
+//        if (j % 2 == 0)
+//            Console.WriteLine(j);
+
+
+//}
+//if (n < m)
+//{
+//    for (i = n; i < m; i++)
+
+
+//        total += i;
+
+
+//    for (j = 1; j < total; j++)
+//        if (j % 2 == 0)
+//            Console.WriteLine(j);
+
+
+//}
+//else if (m == n)
+//{
+//    Console.WriteLine("Daxil olan ededler bir-birine beraberdir");
+//}
+
+
+
+
+
